dd96408de7c7064c93599a4dd6142b099d9edbe3
Herrin <herrinp@amazon.com>
2017-10-21 20:14:34 -0500

    0.11 added export to csv feature

--- coa/urls.py
+++ coa/urls.py
@@ -2,12 +2,12 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
-from coa.views import coa_home, add_coa
+from coa.views import coa_home, add_coa, export_coa_weekToDate_csv
 
 
 urlpatterns = [
     url(r'^$', coa_home),
     url(r'^coa/$', coa_home),
-    url(r'^add_coa/$', add_coa)
-
+    url(r'^add_coa/$', add_coa),
+    url(r'^export/csv/$', export_coa_weekToDate_csv, name='export_coa_weekToDate_csv'),
 ]
--- coa/views.py
+++ coa/views.py
@@ -6,6 +6,8 @@ from .models import facilityGoal, coaEntry, coaQuestionBank
 from django.template import RequestContext
 from django_tables2 import RequestConfig
 from .tables import coaEntryTable
+from django.http import HttpResponse
+import csv
 
 # Create your views here.
 
@@ -67,3 +69,42 @@ def add_coa(request):
         # Render the form with error messages (if any).
 
     return render(request, 'coa_entry.html', {'form': form}, context)
+
+def export_coa_weekToDate_csv(request):
+        startOfWeekDate = (datetime.today() - timedelta(days=(1 + datetime.today().weekday())))
+        import csv
+        from django.utils.encoding import smart_str
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=weekToDateCOA.csv'
+        writer = csv.writer(response, csv.excel)
+        response.write(u'\ufeff'.encode('utf8'))  # BOM (optional...Excel needs it to open UTF-8 file properly)
+        writer.writerow([
+            smart_str(u"Date"),
+            smart_str(u"Site"),
+            smart_str(u"Interviewer"),
+            smart_str(u"Associate Login"),
+            smart_str(u"Question 1"),
+            smart_str(u"Answer 1"),
+            smart_str(u"Question 2"),
+            smart_str(u"Answer 2"),
+            smart_str(u"Question 3"),
+            smart_str(u"Answer 3"),
+
+        ])
+        queryset = coaEntry.objects.all().filter(dateEntered__gte=startOfWeekDate, facility=request.session['facility'])
+        for obj in queryset:
+            writer.writerow([
+                smart_str(obj.dateEntered),
+                smart_str(obj.facility),
+                smart_str(obj.enteringUser),
+                smart_str(obj.askedUser),
+                smart_str(obj.questionOne),
+                smart_str(obj.answerOne),
+                smart_str(obj.questionTwo),
+                smart_str(obj.answerTwo),
+                smart_str(obj.questionThree),
+                smart_str(obj.answerThree),
+            ])
+        return response
+
+
